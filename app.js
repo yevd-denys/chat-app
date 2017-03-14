581c28d0a7a6dd57a2d7f8870bf8d435c3311e15
dyevduschenko <dyevduschenko>
2017-03-14 09:10:03 +0200

    add templates/layout and make

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var log = require('./libs/log')(module);
 var app = express();
 
 // view engine setup
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', config.get('port'));
