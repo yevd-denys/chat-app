086aee2c15c19f4b21480a1a4733fce956028e5c
dyevduschenko <dyevduschenko>
2017-03-14 12:40:16 +0200

    use mongoose for MongoDB, add user model

--- createDb.js
+++ createDb.js
@@ -1,22 +1,44 @@
-var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
+var mongoose = require('./libs/mongoose');
+var async = require('async'); // or promises
 
-MongoClient.connect('mongodb://127.0.0.1:27017/chat', function(err, db) {
-    if (err) throw err;
 
-    var collection = db.collection('test_insert');
-    collection.remove({}, function(err, affected) {
-        if (err) throw err;
-
-        collection.insert({a: 2}, function(err, docs) {
-
-            var cursor = collection.find({a: 2});
-            cursor.toArray(function(err, results) {
-                console.dir(results);
-                // Let's close the db
-                db.close();
-            });
-        });
-
-    });
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createUsers
+], function (err, results) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255 : 0);
 });
+
+function open(callback) {
+    mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+    require('./models/user');
+
+    async.each(Object.keys(mongoose.models), function (modelName, callback) {
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createUsers(callback){
+    users = [
+        {username: "Вася", password: "supervasya"},
+        {username: "Петя", password: "123"},
+        {username: "admin", password: "thetruehero"}
+    ];
+
+    async.each(users, function(userData, callback) {
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
+    }, callback);
+}
